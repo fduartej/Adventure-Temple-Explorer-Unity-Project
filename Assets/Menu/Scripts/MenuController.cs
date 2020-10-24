f3e90fe05e88279f8044652e65932db0179672a3
Fred Duarte <gestion_atlassian@inteligogroup.com>
2020-10-24 13:42:26 -0500

    Agregando el Menu

---
+++ Assets/Menu/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public Button btnPlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnPlay.onClick.AddListener(StartGame);
+    }
+
+    void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+}
